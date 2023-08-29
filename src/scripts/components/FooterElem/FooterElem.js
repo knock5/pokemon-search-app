1b19da2ed98c2eb043912d19fe1a4ec92829c2d5
knock5 <miftachulhd5@gmail.com>
2023-08-29 22:30:54 +0700

    mod: fixing link data source api

--- src/scripts/components/FooterElem/FooterElem.js
+++ src/scripts/components/FooterElem/FooterElem.js
@@ -6,7 +6,7 @@ class FooterElem extends HTMLElement {
   render() {
     this.innerHTML = `
       <div class="flex wrap-foot">
-        <div class="foot-item">All this data obtained from : <a class="foot-link" href="https://pokeapi.co/api/v2/pokemon/">Poke API</a></div>
+        <div class="foot-item">All this data obtained from : <a class="foot-link" href="https://pokeapi.co/">Poke API</a></div>
         <div class="foot-item">
           <p>Copyright &copy; 2023 Miftachul Huda</p>
         </div>
