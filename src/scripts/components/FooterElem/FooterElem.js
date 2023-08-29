ed46dc3149b853d6c0f187b65af1cb63cbad7369
knock5 <miftachulhd5@gmail.com>
2023-08-29 22:44:25 +0700

    mod: fixing target link footer

--- src/scripts/components/FooterElem/FooterElem.js
+++ src/scripts/components/FooterElem/FooterElem.js
@@ -6,12 +6,12 @@ class FooterElem extends HTMLElement {
   render() {
     this.innerHTML = `
       <div class="flex wrap-foot">
-        <div class="foot-item">All this data obtained from : <a class="foot-link" href="https://pokeapi.co/">Poke API</a></div>
+        <div class="foot-item">All this data obtained from : <a class="foot-link" target="_blank" href="https://pokeapi.co/">Poke API</a></div>
         <div class="foot-item">
           <p>Copyright &copy; 2023 Miftachul Huda</p>
         </div>
         <div class="foot-item">
-          Wanna say Hi? <a class="foot-link" href="https://www.instagram.com/miftachulhd/">Click here</a> 
+          Wanna say Hi? <a class="foot-link" target="_blank" href="https://www.instagram.com/miftachulhd/">Click here</a> 
         </div>
       </div>
     `;
